57593f2e9018f7f0a47c3e7af161e435172eb3ee
Konopka-Wojciech <161934780+Konopka-Wojciech@users.noreply.github.com>
2024-03-23 19:33:07 +0100

    Add files via upload
    Zadanie 2 z wzorca Singleton

---
+++ Singleton zad2/Buffer.cs
@@ -0,0 +1,55 @@
+﻿
+
+namespace Singleton_zad2
+{
+    public sealed class Buffer
+    {
+        private static Buffer? _instance;
+        private List<string> printBuffer;
+        private Buffer()
+        {
+           printBuffer = new List<string>();
+        }
+
+        public static Buffer getPrintBuffer()
+        {
+            if (_instance == null)
+            {
+                _instance = new Buffer();
+            }
+            return _instance;
+
+        }
+
+        public void addFile(string file)
+        {
+            printBuffer.Add(file);
+        }
+
+
+        public void Print() 
+        {
+            if (printBuffer.Count > 0)
+            {
+                Console.WriteLine("Printing:");
+                foreach (var file in printBuffer)
+                {
+                    Console.WriteLine(file);
+
+                }
+                printBuffer.Clear();
+            }
+            else 
+            {   
+                Console.WriteLine("Buffer is empty");
+            }
+        }
+        public void clear() 
+        {
+            printBuffer.Clear();
+        }
+
+    }
+
+}
+
---
+++ Singleton zad2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton_zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send items to printer");
+
+            Buffer buffer = Buffer.getPrintBuffer();
+
+            buffer.addFile("file 1");
+            buffer.addFile("file 2");
+            buffer.Print();
+            buffer.Print();
+
+        }
+    }
+}
