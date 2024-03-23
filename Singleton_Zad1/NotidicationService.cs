b2ddaf5789c49b2476b585bc2e2192b91f464ed2
Konopka-Wojciech <161934780+Konopka-Wojciech@users.noreply.github.com>
2024-03-23 17:25:47 +0100

    Add files via upload

---
+++ Singleton_Zad1/NotidicationService.cs
@@ -0,0 +1,65 @@
+﻿
+
+namespace Singleton_Zad1
+{
+    public sealed class NotificationService
+    {
+
+
+        private  List<string> Logs;
+
+        private static  NotificationService? _notificationService;
+        private NotificationService()
+        {
+            Logs = new List<string>();
+        }
+
+        public static NotificationService getNotificationService()
+        {
+            if (_notificationService == null)
+            {
+                _notificationService = new NotificationService();
+            }
+
+            return _notificationService;
+        }
+
+        private  Dictionary<int, string> Notes = new Dictionary<int, string>()
+        {
+            {404,"Not found"},
+            {502,"Bad gateway"},
+            {504,"Gateway timeout"}
+        };
+        
+        public  void showNotification(int code)
+        {
+            if (code == 404)
+            {
+                Console.WriteLine(Notes.GetValueOrDefault(404));
+                Logs.Add(DateTime.Now.ToString()+" "+Notes.GetValueOrDefault(404));
+            }
+            if (code == 504)
+            {
+                Console.WriteLine(Notes.GetValueOrDefault(504));
+                Logs.Add(DateTime.Now.ToString() + " " + Notes.GetValueOrDefault(504));
+            }
+            if (code == 502)
+            {
+                Console.WriteLine(Notes.GetValueOrDefault(502));
+                Logs.Add(DateTime.Now.ToString() + " " + Notes.GetValueOrDefault(502));
+            }
+
+
+
+        }
+        
+        public  void getAllLogs()
+        {
+            foreach (var item in Logs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ Singleton_Zad1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Singleton_Zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            NotificationService Service = NotificationService.getNotificationService();
+            
+
+            List<int> codes = new List<int>();
+            Random rnd = new Random();
+            codes.Add(404);
+            codes.Add(502);
+            codes.Add(504);
+
+            Console.WriteLine("Serwis do obsługi komunikatów");
+            for (int i = 0; i < 5; i++) 
+            {
+                Service.showNotification(codes[rnd.Next(0, 3)]);
+            }
+
+            Service.getAllLogs();
+            
+            Console.ReadKey();
+        }
+    }
+}
